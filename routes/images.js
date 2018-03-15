d3640e24183567480303950501c331760946273f
juanmaperez <juanmaperezvar@gmail.com>
2018-03-15 21:13:06 +0100

    edit profile done

--- routes/images.js
+++ routes/images.js
@@ -3,20 +3,20 @@ const express = require('express')
 const imagesRouter = express();
 const fs = require('fs');
 
+const pathManager = require('path');
+
 imagesRouter.get('/:collection/:img', (req, res, next)=>{
 
     const collection = req.params.collection;
     const img = req.params.img;
 
-    const path = `./uploads/${collection}/${img}`;
+    let path = `./uploads/${collection}/${img}`;
 
-    fs.exists(path, image=>{
-        if(!image){
-            path = './assets/no-image.jpg';
-        }
-    })
+    if( !fs.existsSync(path) ) {
+        path = './assets/no-img.jpg';
+    }
 
-    res.sendFile(path);
+    res.sendFile(pathManager.resolve(path));
 })
 
 module.exports = imagesRouter;
